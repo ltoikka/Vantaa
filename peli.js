b1655e903db3bc72ffe2528a6f48ee7795b59358
saanios1 <salla-marisaanio@Salla-Maris-MacBook-Pro.local>
2016-04-26 10:03:39 +0300

    lisää kamaaa

--- peli.js
+++ peli.js
@@ -25,6 +25,35 @@ $(document).ready( function() {
     canvas.add(oImg);
   });
 
+fabric.Image.fromURL('https://pixabay.com/static/uploads/photo/2014/12/21/23/44/table-575934_960_720.png', function(oImg) {
+    oImg.scale(0.1).set({
+      left: 100,
+      top: 100,
+      angle: 30,
+      opacity: 1
+    });
+    canvas.add(oImg);
+  });
+
+fabric.Image.fromURL('curtainleft.png', function(oImg) {
+    oImg.scale(0.1).set({
+      left: 100,
+      top: 100,
+      angle: 30,
+      opacity: 1
+    });
+    canvas.add(oImg);
+  });
+
+fabric.Image.fromURL('curtainright.png', function(oImg) {
+    oImg.scale(0.1).set({
+      left: 100,
+      top: 100,
+      angle: 30,
+      opacity: 1
+    });
+    canvas.add(oImg);
+  });
 
 canvas.setBackgroundImage('junanakyma1.png', canvas.renderAll.bind(canvas), {
     backgroundImageOpacity: 0,
