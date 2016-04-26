e4dc89853eb6999a107eec31d8dc9c6279254f14
saanios1 <salla-marisaanio@Salla-Maris-MacBook-Pro.local>
2016-04-26 10:09:26 +0300

    kamaa

--- peli.js
+++ peli.js
@@ -55,6 +55,47 @@ fabric.Image.fromURL('curtainright.png', function(oImg) {
     canvas.add(oImg);
   });
 
+
+fabric.Image.fromURL('https://pixabay.com/static/uploads/photo/2014/04/02/16/21/table-307005_960_720.png', function(oImg) {
+    oImg.scale(0.1).set({
+      left: 100,
+      top: 100,
+      angle: 30,
+      opacity: 1
+    });
+    canvas.add(oImg);
+  });
+
+fabric.Image.fromURL('https://pixabay.com/static/uploads/photo/2012/04/18/04/19/chair-36810_960_720.png', function(oImg) {
+    oImg.scale(0.1).set({
+      left: 100,
+      top: 100,
+      angle: 30,
+      opacity: 1
+    });
+    canvas.add(oImg);
+  });
+
+fabric.Image.fromURL('https://pixabay.com/static/uploads/photo/2014/12/21/23/47/sofa-576032_960_720.png', function(oImg) {
+    oImg.scale(0.1).set({
+      left: 100,
+      top: 100,
+      angle: 30,
+      opacity: 1
+    });
+    canvas.add(oImg);
+  });
+
+fabric.Image.fromURL('https://pixabay.com/static/uploads/photo/2014/12/21/23/49/bench-576130_960_720.png', function(oImg) {
+    oImg.scale(0.1).set({
+      left: 100,
+      top: 100,
+      angle: 30,
+      opacity: 1
+    });
+    canvas.add(oImg);
+  });
+
 canvas.setBackgroundImage('junanakyma1.png', canvas.renderAll.bind(canvas), {
     backgroundImageOpacity: 0,
     backgroundImageStretch: false
