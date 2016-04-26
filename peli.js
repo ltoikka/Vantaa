ca9b90995496c1ca2b26edc82a1b8c49aa8c97e0
saanios1 <salla-marisaanio@Salla-Maris-MacBook-Pro.local>
2016-04-26 09:41:23 +0300

    pikkukisu

--- peli.js
+++ peli.js
@@ -4,7 +4,17 @@ $(document).ready( function() {
   var canvas = new fabric.Canvas('c');
 
   fabric.Image.fromURL('cattt.jpg', function(oImg) {
-    oImg.set({
+    oImg.scale(0.1).set({
+      left: 100,
+      top: 100,
+      angle: 30,
+      opacity: 0.85
+    });
+    canvas.add(oImg);
+  });
+
+  fabric.Image.fromURL('cattt.jpg', function(oImg) {
+    oImg.scale(0.1).set({
       left: 100,
       top: 100,
       angle: 30,
