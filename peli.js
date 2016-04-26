b17eb82ac86e0a314ec2408aba2ecad39f473c83
saanios1 <salla-marisaanio@Salla-Maris-MacBook-Pro.local>
2016-04-26 13:08:42 +0300

    kuntoon

--- peli.js
+++ peli.js
@@ -5,9 +5,8 @@ $(document).ready( function() {
   var canvas = new fabric.Canvas('c');
   var ctx = canvas.getContext('2d');
 
-<<<<<<< Updated upstream
   var startScreen = true;
-=======
+
   var x = document.getElementById("myAudio"); 
 
 function playAudio() { 
@@ -21,36 +20,6 @@ function pauseAudio() {
 $('#play').click(playAudio)
 $('#pause').click(pauseAudio)
 
-  fabric.Image.fromURL('https://pixabay.com/static/uploads/photo/2014/04/03/10/00/stool-309589_960_720.png', function(oImg) {
-    oImg.scale(0.1).set({
-      left: 10,
-      top: 535,
-      angle: 0,
-      opacity: 1
-    });
-    canvas.add(oImg);
-  });
-
-  fabric.Image.fromURL('cattt.jpg', function(oImg) {
-    oImg.scale(0.1).set({
-      left: 130,
-      top: 535,
-      angle: 0,
-      opacity: 1
-    });
-    canvas.add(oImg);
-  });
-
-fabric.Image.fromURL('https://pixabay.com/static/uploads/photo/2014/12/21/23/44/table-575934_960_720.png', function(oImg) {
-    oImg.scale(0.1).set({
-      left: 250,
-      top: 535,
-      angle: 0,
-      opacity: 1
-    });
-    canvas.add(oImg);
-  });
->>>>>>> Stashed changes
 
   canvas.on('mouse:down', function() {
     canvas.isDrawingMode = true;
